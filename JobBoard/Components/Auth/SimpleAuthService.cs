981653916b8ca085945f27b0919e8f3ef3a1ea89
ROYCE NEO 2404126A <2404126A@student.tp.edu.sg>
2025-12-14 22:40:10 +0800

    Added temporary login page and auth service

---
+++ JobBoard/Components/Auth/SimpleAuthService.cs
@@ -0,0 +1,29 @@
+﻿namespace JobBoard.Components.Auth
+{
+    public class SimpleAuthService
+    {
+        public bool IsLoggedIn { get; private set; } = false;
+        public string? CurrentUserEmail { get; private set; }
+
+        private const string HardEmail = "test@jobboard.com";
+        private const string HardPassword = "123456";
+
+        public bool Login(string email, string password)
+        {
+            if (email == HardEmail && password == HardPassword)
+            {
+                IsLoggedIn = true;
+                CurrentUserEmail = email;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+            CurrentUserEmail = null;
+        }
+    }
+}
--- JobBoard/Program.cs
+++ JobBoard/Program.cs
@@ -1,10 +1,14 @@
 using JobBoard.Components;
+using JobBoard.Components.Auth;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<SimpleAuthService>();
 
 var app = builder.Build();
 
